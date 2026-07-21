cb714430264eb3541acd4defed89ed7857ac8ef2
SufiyanKhanCloud <sufiikhan980@gmail.com>
2026-07-21 15:10:13 -0400

    Add final newline to routes.py

--- service/routes.py
+++ service/routes.py
@@ -91,7 +91,7 @@ def get_accounts(account_id):
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
 
-    return account.serialize(), status.HTTP_200_OK  # place your code here to READ an account
+    return account.serialize(), status.HTTP_200_OK
 
 
 ######################################################################
